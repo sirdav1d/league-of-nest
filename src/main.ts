f97f755ff3de9da706da8824778d57c16ffd2f5f
sirdav1d <ddavid.diniz@gmail.com>
2022-11-28 21:43:16 -0300

    chore:cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
