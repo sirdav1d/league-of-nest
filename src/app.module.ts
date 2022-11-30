f868e6a0bdb006c92d1043ada8cabe3695e616b4
sirdav1d <ddavid.diniz@gmail.com>
2022-11-30 20:51:18 -0300

    remove: skill module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ChampionModule } from './champion/champion.module';
-import { SkillModule } from './skill/skill.module';
 import { DutyModule } from './duty/duty.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ChampionModule, SkillModule, DutyModule],
+  imports: [PrismaModule, UserModule, ChampionModule, DutyModule],
   controllers: [AppController],
   providers: [AppService],
 })
