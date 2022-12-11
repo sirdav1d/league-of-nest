1608d802ab7858d8f2062dba1d05f508c5a2e15f
sirdav1d <ddavid.diniz@gmail.com>
2022-12-11 04:10:03 -0300

    feat(auth): implements user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { IUser } from 'src/user/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 import { LoginResponsDto } from './dto/auth.response.dto';
+import { LoggedUser } from './Logged.user.decorator';
 
 @Controller('auth')
 @ApiTags('Auth')
@@ -19,14 +29,13 @@ export class AuthController {
     return this.authService.login(LoginDto);
   }
 
-
   @Get()
   @UseGuards(AuthGuard())
   @ApiOperation({
-    summary: 'Retorna o usuário logado no momento'
+    summary: 'Retorna o usuário logado no momento',
   })
   @ApiBearerAuth()
-  profile(){
-    return 'validado'
+  profile(@LoggedUser() user: IUser) {
+    return user;
   }
 }
